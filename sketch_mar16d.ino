ec8049b9ad55c9ec7c3513f1d8b588ab3e385bd4
reishim <44530189+greenteabiscuit@users.noreply.github.com>
2021-03-21 17:35:06 +0900

    Add files via upload

--- sketch_mar16d.ino
+++ sketch_mar16d.ino
@@ -1,10 +1,27 @@
 //Blynk で リモートLチカ
-
+// Blynk
 #define BLYNK_PRINT Serial
 #define BLYNK_USE_DIRECT_CONNECT
 #include <BlynkSimpleEsp32_BLE.h>
 #include <BLEDevice.h>
 #include <BLEServer.h>
+
+// environment sensor
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BME680.h"
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME680 bme; // I2C
+
+// Blynk authentication
 char auth[] = "Lq2QhC0nSE79FTHRxu5YxbSTwGkjxLDY";
 
 BlynkTimer timer;
@@ -13,14 +30,43 @@ void myTimerEvent()
 {
   // You can send any value at any time.
   // Please don't send more that 10 values per second.
+
   long x , y , z ;
-  x = analogRead(33); // Ｘ軸
-  y = analogRead(34);
-  z = analogRead(35);
+  x = analogRead(32); // Ｘ軸
+  y = analogRead(35);
+  z = analogRead(34);
   Blynk.virtualWrite(V5, x);
   Blynk.virtualWrite(V6, y);
   Blynk.virtualWrite(V7, z);
+  Blynk.virtualWrite(V8, bme.temperature);
+  Blynk.virtualWrite(V9, bme.pressure);
   Serial.println(z);
+  if (! bme.performReading()) {
+    Serial.println("Failed to perform reading :(");
+    return;
+  }
+  Serial.print("Temperature = ");
+  Serial.print(bme.temperature);
+  Serial.println(" *C");
+
+  Serial.print("Pressure = ");
+  Serial.print(bme.pressure / 100.0);
+  Serial.println(" hPa");
+
+  Serial.print("Humidity = ");
+  Serial.print(bme.humidity);
+  Serial.println(" %");
+
+  Serial.print("Gas = ");
+  Serial.print(bme.gas_resistance / 1000.0);
+  Serial.println(" KOhms");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+
+  Serial.println();
+  delay(2000);
 }
 
 void setup(){
@@ -31,6 +77,17 @@ void setup(){
   Serial.println("Waiting for connections...");
   Blynk.setDeviceName("L-chika");
   Blynk.begin(auth);
+  if (!bme.begin()) {
+    Serial.println("Could not find a valid BME680 sensor, check wiring!");
+    while (1);
+  }
+
+  // Set up oversampling and filter initialization
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150); // 320*C for 150 ms
   timer.setInterval(1000L, myTimerEvent);
 }
 
