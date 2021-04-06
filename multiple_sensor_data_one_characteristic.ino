729d77d205a3000084c590efd43bdd6c39ae5093
reishim <44530189+greenteabiscuit@users.noreply.github.com>
2021-04-06 11:14:03 +0900

    Add files via upload

---
+++ multiple_sensor_data_one_characteristic.ino
@@ -0,0 +1,104 @@
+#include <Nefry.h>
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+#include <Wire.h>
+#include <SPI.h>
+
+uint8_t accel_sensor_value_x = 0;
+uint8_t accel_sensor_value_y = 0;
+uint8_t accel_sensor_value_z = 0;
+
+bool deviceConnected = false;
+
+#define NUMBER_OF_SENSORS 4
+
+union multi_sensor_data
+{
+  struct __attribute__( ( packed ) )
+  {
+    float values[NUMBER_OF_SENSORS];
+  };
+  uint8_t bytes[ NUMBER_OF_SENSORS * sizeof( float ) ];
+};
+
+union multi_sensor_data multiSensorData;
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID        "d5875408-fa51-4763-a75d-7d33cecebc31"
+#define CHARACTERISTIC_UUID "a4f01d8c-a037-43b6-9050-1876a8c23584"
+
+BLECharacteristic *multiSensorDataCharacteristic;
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+void setup() {
+  Serial.begin(115200);
+
+  // Create the BLE Device
+  BLEDevice::init("NefryBT");
+
+  // Create the BLE Server
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  multiSensorDataCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+
+  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+  // Create a BLE Descriptor
+  multiSensorDataCharacteristic->addDescriptor(new BLE2902());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  pServer->getAdvertising()->start();
+  Serial.println("Waiting a client connection to notify...");
+}
+
+void loop() {
+
+  if (deviceConnected) {
+    accel_sensor_value_x = analogRead(32);
+    accel_sensor_value_y = analogRead(35);
+    accel_sensor_value_z = analogRead(34);
+    // Serial.printf("*** NOTIFY: %d ***\n", value);
+    Serial.printf("*** NOTIFY: %d ***\n", accel_sensor_value_x);
+    char buffer[32];
+    // sprintf(buffer, "{\"val\":%d}", value);
+    sprintf(buffer, "{\"val_accel_x\":%d}", accel_sensor_value_x);
+    Serial.printf(buffer);
+
+    sprintf(buffer, "{\"val_accel_y\":%d}", accel_sensor_value_y);
+    Serial.printf(buffer);
+
+    sprintf(buffer, "{\"val_accel_z\":%d}", accel_sensor_value_z);
+    Serial.printf(buffer);
+    multiSensorDataCharacteristic->setValue( multiSensorData.bytes, sizeof multiSensorData.bytes );
+    multiSensorDataCharacteristic->notify();
+
+  }
+  delay(2000);
+}
