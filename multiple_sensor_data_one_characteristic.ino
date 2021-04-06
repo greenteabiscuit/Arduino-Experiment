55c44bb367a2682c487d96b98f3d4bdad6e72ca4
reishim <44530189+greenteabiscuit@users.noreply.github.com>
2021-04-06 18:15:16 +0900

    Add files via upload

--- multiple_sensor_data_one_characteristic.ino
+++ multiple_sensor_data_one_characteristic.ino
@@ -1,3 +1,5 @@
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BME680.h"
 
 #include <BLEDevice.h>
 #include <BLEServer.h>
@@ -12,6 +14,13 @@ uint8_t accel_sensor_value_y = 0;
 uint8_t accel_sensor_value_z = 0;
 
 bool deviceConnected = false;
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+#define I2C_SDA 23
+#define I2C_SCL 22
+TwoWire I2CBME = TwoWire(0);
+Adafruit_BME680 bme(&I2CBME); // I2C
 
 #define NUMBER_OF_SENSORS 4
 
@@ -45,10 +54,20 @@ class MyServerCallbacks: public BLEServerCallbacks {
 };
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
+  I2CBME.begin(I2C_SDA, I2C_SCL, 100000);
+  if (!bme.begin()) {
+    Serial.println("Could not find a valid BME680 sensor, check wiring!");
+  }
+  // Set up oversampling and filter initialization
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150); // 320*C for 150 ms
 
   // Create the BLE Device
-  BLEDevice::init("NefryBT");
+  BLEDevice::init("Up Bluetooth Device");
 
   // Create the BLE Server
   BLEServer *pServer = BLEDevice::createServer();
@@ -110,6 +129,27 @@ void loop() {
     
     
     multiSensorDataCharacteristic->notify();
+    Serial.print("Temperature = ");
+    Serial.print(bme.temperature);
+    Serial.println(" *C");
+  
+    Serial.print("Pressure = ");
+    Serial.print(bme.pressure / 100.0);
+    Serial.println(" hPa");
+  
+    Serial.print("Humidity = ");
+    Serial.print(bme.humidity);
+    Serial.println(" %");
+  
+    Serial.print("Gas = ");
+    Serial.print(bme.gas_resistance / 1000.0);
+    Serial.println(" KOhms");
+  
+    Serial.print("Approx. Altitude = ");
+    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+    Serial.println(" m");
+  
+    Serial.println();
 
   }
   delay(2000);
